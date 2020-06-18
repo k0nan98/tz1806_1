15b59492c3c399792e2d531f95fbccf1b9f69097
K0nan98 <k0nan98warwar@gmail.com>
2020-06-18 16:47:17 +0300

    Initial comment

---
+++ theTask/Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject playerObject;
+
+    float x_offset = 0;
+    private void Start()
+    {
+        x_offset = playerObject.transform.position.x - mainCamera.transform.position.x;
+    }
+    void Update()
+    {
+        if(playerObject.transform.position.x - mainCamera.transform.position.x < x_offset)
+        {
+            mainCamera.transform.position = new Vector3(playerObject.transform.position.x-x_offset, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        }
+        
+    }
+}
